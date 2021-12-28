6ea97dc560d87acd66edbb53f27c089bad7dcfe5
Patrick Lehmann <Patrick.Lehmann@plc2.de>
2021-12-28 21:08:35 +0100

    Added CLIOption decorators.

--- pyEDAA/CLITool/Mentor/ModelSim.py
+++ pyEDAA/CLITool/Mentor/ModelSim.py
@@ -6,8 +6,7 @@ from pyTooling.Decorators import export
 from pyEDAA.CLITool import ToolMixIn
 from pyTooling.CLIAbstraction            import CLIOption, Executable
 from pyTooling.CLIAbstraction.Argument import (
-	ShortFlagArgument, LongFlagArgument,
-	ShortValuedFlagArgument, LongValuedFlagArgument,
+	ShortFlagArgument,
 	StringArgument, OptionalValuedFlagArgument, ShortTupleArgument
 )
 
@@ -19,6 +18,7 @@ class VHDLLibraryTool(Executable, ToolMixIn):
 		"Windows": "vlib.exe"
 	}
 
+	@CLIOption
 	class ValueLibraryName(StringArgument): ...
 
 
@@ -65,61 +65,88 @@ class VHDLCompiler(Executable, ToolMixIn):
 		"Windows": "vcom.exe"
 	}
 
+	@CLIOption
 	class FlagTime(ShortFlagArgument, name="time"):
 		"""Print the compilation wall clock time."""
 
+	@CLIOption
 	class FlagExplicit(ShortFlagArgument, name="explicit"): ...
 
+	@CLIOption
 	class FlagQuietMode(ShortFlagArgument, name="quiet"):
 		"""Do not report 'Loading...' messages."""
 
+	@CLIOption
 	class SwitchModelSimIniFile(ShortTupleArgument, name="modelsimini"): ...
 
+	@CLIOption
 	class FlagRangeCheck(ShortFlagArgument, name="rangecheck"): ...
 
+	@CLIOption
 	class SwitchCoverage(OptionalModelSimPlusArgument, name="cover"): ...
 
+	@CLIOption
 	class FlagEnableFocusedExpressionCoverage(ShortFlagArgument, name="coverfec"): ...
 
+	@CLIOption
 	class FlagDisableFocusedExpressionCoverage(ShortFlagArgument, name="nocoverfec"): ...
 
+	@CLIOption
 	class FlagEnableRapidExpressionCoverage(ShortFlagArgument, name="coverrec"): ...
 
+	@CLIOption
 	class FlagDisableRapidExpressionCoverage(ShortFlagArgument, name="nocoverrec"): ...
 
+	@CLIOption
 	class FlagEnableRecognitionOfImplicitFSMResetTransitions(ShortFlagArgument, name="fsmresettrans"): ...
 
+	@CLIOption
 	class FlagDisableRecognitionOfImplicitFSMResetTransitions(ShortFlagArgument, name="nofsmresettrans"): ...
 
+	@CLIOption
 	class FlagEnableRecognitionOfSingleBitFSMState(ShortFlagArgument, name="fsmsingle"): ...
 
+	@CLIOption
 	class FlagDisableRecognitionOfSingleBitFSMState(ShortFlagArgument, name="nofsmsingle"): ...
 
+	@CLIOption
 	class FlagEnableRecognitionOfImplicitFSMTransitions(ShortFlagArgument, name="fsmimplicittrans"): ...
 
+	@CLIOption
 	class FlagDisableRecognitionOfImplicitFSMTransitions(ShortFlagArgument, name="nofsmimplicittrans"): ...
 
+	@CLIOption
 	class SwitchFSMVerbosityLevel(OptionalModelSimMinusArgument, name="fsmverbose"): ...
 
+	@CLIOption
 	class FlagReportAsNote(ShortTupleArgument, name="note"): ...
 
+	@CLIOption
 	class FlagReportAsError(ShortTupleArgument, name="error"): ...
 
+	@CLIOption
 	class FlagReportAsWarning(ShortTupleArgument, name="warning"): ...
 
+	@CLIOption
 	class FlagReportAsFatal(ShortTupleArgument, name="fatal"): ...
 
+	@CLIOption
 	class FlagRelaxLanguageChecks(ShortFlagArgument, name="permissive"): ...
 
+	@CLIOption
 	class FlagForceLanguageChecks(ShortFlagArgument, name="pedanticerrors"): ...
 
+	@CLIOption
 	class SwitchVHDLVersion(StringArgument, pattern="-{0}"): ...
 
+	@CLIOption
 	class ArgLogFile(ShortTupleArgument, name="l"): ...			# what's the difference to -logfile ?
 
+	@CLIOption
 	class SwitchVHDLLibrary(ShortTupleArgument, name="work"): ...
 
-	class ArgSourceFile(PathArgument): ...
+	# @CLIOption
+	# class ArgSourceFile(PathArgument): ...
 
 
 class VHDLSimulator(Executable, ToolMixIn):
@@ -128,72 +155,100 @@ class VHDLSimulator(Executable, ToolMixIn):
 		"Windows": "vsim.exe"
 	}
 
+	@CLIOption
 	class FlagQuietMode(ShortFlagArgument, name="quiet"):
 		"""Run simulation in quiet mode. (Don't show 'Loading...' messages."""
 
+	@CLIOption
 	class FlagBatchMode(ShortFlagArgument, name="batch"):
 		"""Run simulation in batch mode."""
 
+	@CLIOption
 	class FlagGuiMode(ShortFlagArgument, name="gui"):
 		"""Run simulation in GUI mode."""
 
+	@CLIOption
 	class SwitchBatchCommand(ShortTupleArgument, name="do"):
 		"""Specify a Tcl batch script for the batch mode."""
 
+	@CLIOption
 	class FlagCommandLineMode(ShortFlagArgument, name="c"):
 		"""Run simulation in command line mode."""
 
+	@CLIOption
 	class SwitchModelSimIniFile(ShortTupleArgument, name="modelsimini"):
 		"""Specify the used 'modelsim.ini' file."""
 
+	@CLIOption
 	class FlagEnableOptimization(ShortFlagArgument, name="vopt"):
 		"""Enabled optimization while elaborating the design."""
 
+	@CLIOption
 	class FlagDisableOptimization(ShortFlagArgument, name="novopt"):
 		"""Disabled optimization while elaborating the design."""
 
+	@CLIOption
 	class FlagEnableOptimizationVerbosity(ShortFlagArgument, name="vopt_verbose"):
 		"""Enabled optimization while elaborating the design."""
 
+	@CLIOption
 	class FlagEnableKeepAssertionCountsForCoverage(ShortFlagArgument, name="assertcover"): ...
 
+	@CLIOption
 	class FlagDisableKeepAssertionCountsForCoverage(ShortFlagArgument, name="noassertcover"): ...
 
+	@CLIOption
 	class FlagEnableCoverage(ShortFlagArgument, name="coverage"): ...
 
+	@CLIOption
 	class FlagDisableCoverage(ShortFlagArgument, name="nocoverage"): ...
 
+	@CLIOption
 	class FlagEnablePSL(ShortFlagArgument, name="psl"): ...
 
+	@CLIOption
 	class FlagDisablePSL(ShortFlagArgument, name="nopsl"): ...
 
+	@CLIOption
 	class FlagEnableFSMDebugging(ShortFlagArgument, name="fsmdebug"): ...
 
+	@CLIOption
 	class FlagReportAsNote(ShortTupleArgument, name="note"): ...
 
+	@CLIOption
 	class FlagReportAsError(ShortTupleArgument, name="error"): ...
 
+	@CLIOption
 	class FlagReportAsWarning(ShortTupleArgument, name="warning"): ...
 
+	@CLIOption
 	class FlagReportAsFatal(ShortTupleArgument, name="fatal"): ...
 
+	@CLIOption
 	class FlagRelaxLanguageChecks(ShortFlagArgument, name="permissive"): ...
 
+	@CLIOption
 	class FlagForceLanguageChecks(ShortFlagArgument, name="pedanticerrors"): ...
 
+	@CLIOption
 	class SwitchTimeResolution(ShortTupleArgument, name="t"):
 		"""Set simulation time resolution."""
 		# -t [1|10|100]fs|ps|ns|us|ms|sec  Time resolution limit
 
+	@CLIOption
 	class ArgLogFile(ShortTupleArgument, name="l"): ...
 		# what's the difference to -logfile ?
 
+	@CLIOption
 	class ArgKeepStdOut(ShortFlagArgument, name="keepstdout"): ...
 
+	@CLIOption
 	class ArgVHDLLibraryName(ShortTupleArgument, name="lib"): ...
 
+	@CLIOption
 	class ArgOnFinishMode(ShortTupleArgument, name="onfinish"):
 		"""Customize the kernel shutdown behavior at the end of simulation; Valid modes: ask, stop, exit, final (Default: ask)."""
 
+	@CLIOption
 	class SwitchTopLevel(StringArgument):
 		"""The top-level for simulation."""
