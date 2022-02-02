b23974faff43213decda64c7289482abd7c35b2e
Patrick Lehmann <Paebbels@gmail.com>
2022-02-02 07:54:07 +0100

    Bumped versions and minor changes.

--- pyEDAA/CLITool/Aldec/__init__.py
+++ pyEDAA/CLITool/Aldec/__init__.py
@@ -11,7 +11,8 @@
 #                                                                                                                      #
 # License:                                                                                                             #
 # ==================================================================================================================== #
-# Copyright 2021-2022 Patrick Lehmann - Boetzingen, Germany                                                            #
+# Copyright 2017-2022 Patrick Lehmann - Boetzingen, Germany                                                            #
+# Copyright 2014-2016 Technische Universität Dresden - Germany, Chair of VLSI-Design, Diagnostics and Architecture     #
 #                                                                                                                      #
 # Licensed under the Apache License, Version 2.0 (the "License");                                                      #
 # you may not use this file except in compliance with the License.                                                     #
@@ -28,52 +29,4 @@
 # SPDX-License-Identifier: Apache-2.0                                                                                  #
 # ==================================================================================================================== #
 #
-"""This module contains the CLI abstraction layer for `GHDL <https://github.com/ghdl/ghdl>`__."""
-from pyTooling.CLIAbstraction            import CLIOption
-from pyTooling.CLIAbstraction            import Executable
-from pyTooling.CLIAbstraction.Argument   import (
-	ExecutableArgument, CommandArgument,
-	ShortFlagArgument, LongFlagArgument,
-	ShortValuedFlagArgument, LongValuedFlagArgument,
-	LongTupleArgument,
-	StringArgument
-)
-
-
-class Docker(Executable):
-	_executableNames = {
-		"Linux":   "docker",
-		"Windows": "docker.exe"
-	}
-
-	# 'version' sub commands and options
-	@CLIOption()
-	class CommandVersion(CommandArgument, name="version"): ...
-
-	# 'container' sub commands and options
-	@CLIOption()
-	class CommandContainer(CommandArgument, name="container"): ...
-
-	@CLIOption()
-	class CommandRun(CommandArgument, name="run"): ...
-
-	@CLIOption()
-	class FlagContainerName(LongTupleArgument, name="name"): ...
-
-	@CLIOption()
-	class FlagRemoveContainer(LongFlagArgument, name="rm"): ...
-
-	@CLIOption()
-	class FlagMount(LongTupleArgument, name="mount"): ...
-
-	@CLIOption()
-	class FlagVolume(LongTupleArgument, name="volume"): ...
-
-	@CLIOption()
-	class ValueImageName(StringArgument): ...
-
-	@CLIOption()
-	class ValueCommand(StringArgument): ...
-
-	@CLIOption()
-	class Executable(ExecutableArgument): ...  # XXX: no argument here
+"""This package contains the CLI abstraction layer for Aldec tools."""
