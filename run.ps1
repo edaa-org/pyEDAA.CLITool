8225de3a880ce9047f13caa046e56e08d9a43564
Patrick Lehmann <Paebbels@gmail.com>
2026-07-07 18:06:06 +0200

    Bumped dependencies.

--- run.ps1
+++ run.ps1
@@ -33,7 +33,7 @@ Param(
 )
 
 $PackageName = "pyEDAA.CLITool"
-$PackageVersion = "0.4.3"
+$PackageVersion = "0.4.4"
 
 # set default values
 $EnableDebug =        [bool]$PSCmdlet.MyInvocation.BoundParameters["Debug"]
